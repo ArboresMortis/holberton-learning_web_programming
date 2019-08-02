f9cf38fa26295367085fb131a06ffb17bfc366df
Gwendolyn <gwentowne2002@gmail.com>
2019-08-02 13:15:44 -0400

    Update 3-my_first_loop.js

--- 5-javascript/3-my_first_loop.js
+++ 5-javascript/3-my_first_loop.js
@@ -1,16 +1,5 @@
-<!doctype html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <script type = "text/javascript">
-    var count = 0;
-    while (count=0;count < 11) {
-      console.log(count);
-      count++;
-    }
-  </script>
-</head>
-<body>
-</body>
-</html>
-+"<br />"
+var x = 0;
+while (x < 11) {
+  console.log(x);
+  count++;
+}
