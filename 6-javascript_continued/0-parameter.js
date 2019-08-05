550bd27522d19c8800e83dc79fb958cdeb11dd8b
Gwendolyn <gwentowne2002@gmail.com>
2019-08-05 09:49:56 -0400

    Update 0-parameter.js

--- 6-javascript_continued/0-parameter.js
+++ 6-javascript_continued/0-parameter.js
@@ -1,2 +1,2 @@
-var first_para = process.argv[0];
-console.log("Here is the first passed parameter: " + first_para);
+let first_para = process.argv[2];
+console.log("Here is the first passed parameter: " + first_para)
